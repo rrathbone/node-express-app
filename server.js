d440fab3a462fe82b56436e846b418c858956b20
rrathbone <rachellerathbone@gmail.com>
2017-02-18 19:15:23 -0800

    set up gulp

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
 app.get('/', (req, res) => {
   res.send('Hello world');
