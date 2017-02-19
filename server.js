080ff65e2c97039b3894914f216e4c03defad491
rrathbone <rachellerathbone@gmail.com>
2017-02-18 23:37:49 -0800

    templating setup

--- server.js
+++ server.js
@@ -5,10 +5,18 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', 'src/views');
+
+// var handlebars = require('express-handlebars')
+// app.engine('hbs', handlebars({extname: '.hbs'}));
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.render('index', {
+    title: 'Hello from render',
+    list: ['a', 'b']
+  });
 });
 
 app.get('/books', (req, res) => {
